a3004261968090c597c1e441701a31460115de6c
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-13 12:25:30 -0500

    add basic actions: get, post, put, delete

--- smurfs/src/states/smurfs/actions.js
+++ smurfs/src/states/smurfs/actions.js
@@ -1,13 +1,29 @@
 /***************************************
   MAIN
 ----------------------------------------
-  - types  : list of action types
-  - make   : fun to make (create) an action obj
-  - makers : obj of action maker (creator) funs
+  - types    : list of action types
+  - make     : fun to make (create) an action obj
+  - makers   : obj of action maker (creator) funs
+  - specials : obj of specialized dispatchers
 ***************************************/
 
 export const types = {
-  DO_SOMETHING : 'DO_SOMETHING',
+  // get
+  GET_VILLAGE_TRY     : 'GET_VILLAGE_TRY',
+  GET_VILLAGE_SUCCESS : 'GET_VILLAGE_SUCCESS',
+  GET_VILLAGE_FAILURE : 'GET_VILLAGE_FAILURE',
+  // post
+  POST_SMURF_TRY     : 'POST_SMURF_TRY',
+  POST_SMURF_SUCCESS : 'POST_SMURF_SUCCESS',
+  POST_SMURF_FAILURE : 'POST_SMURF_FAILURE',
+  // put
+  PUT_SMURF_TRY     : 'PUT_SMURF_TRY',
+  PUT_SMURF_SUCCESS : 'PUT_SMURF_SUCCESS',
+  PUT_SMURF_FAILURE : 'PUT_SMURF_FAILURE',
+  // delete
+  DELETE_SMURF_TRY     : 'DELETE_SMURF_TRY',
+  DELETE_SMURF_SUCCESS : 'DELETE_SMURF_SUCCESS',
+  DELETE_SMURF_FAILURE : 'DELETE_SMURF_FAILURE',
 };
 
 const make = (type, data) => ({ type, data });

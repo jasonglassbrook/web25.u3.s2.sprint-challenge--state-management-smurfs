204ecb7ece02643383feb1d34d58676929e33f83
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-13 12:58:21 -0500

    write "special" actions for http requests

--- smurfs/src/states/smurfs/actions.js
+++ smurfs/src/states/smurfs/actions.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 /***************************************
   MAIN
 ----------------------------------------
@@ -7,6 +9,9 @@
   - specials : obj of specialized dispatchers
 ***************************************/
 
+/*--------------------------------------
+  actions / types
+--------------------------------------*/
 export const types = {
   // get
   GET_VILLAGE_TRY     : 'GET_VILLAGE_TRY',
@@ -26,7 +31,10 @@ export const types = {
   DELETE_SMURF_FAILURE : 'DELETE_SMURF_FAILURE',
 };
 
-const make = (type, data) => ({ type, data });
+/*--------------------------------------
+  actions / make, makers
+--------------------------------------*/
+const make = (type, payload) => ({ type, payload });
 
 export const makers = Object.fromEntries (
   Object.keys (types).map ((name) => [
@@ -34,9 +42,96 @@ export const makers = Object.fromEntries (
   ])
 );
 
+/*--------------------------------------
+  actions / specials
+--------------------------------------*/
+export const specials = {
+  getVillage   : {},
+  addSmurf     : {},
+  editSmurf    : {},
+  deleteSmurf  : {},
+};
+
+const api = {
+  baseURL : 'http://localhost:3333/smurfs',
+  village : {},
+  smurf   : {},
+};
+api.village.get  = () => (api.baseURL);
+api.smurf.post   = () => (api.baseURL);
+api.smurf.put    = (id) => (`${api.baseURL}/${id}`);
+api.smurf.delete = (id) => (`${api.baseURL}/${id}`);
+
+/*------------------
+  getVillage
+------------------*/
+specials.getVillage = () => (dispatch /*, getState */) => {
+  dispatch (make (types.GET_VILLAGE_TRY));
+  axios
+    .get (api.village.get ())
+    .then ((response) => {
+      dispatch (make (types.GET_VILLAGE_SUCCESS, response.data));
+    })
+    .catch ((error) => {
+      dispatch (make (types.GET_VILLAGE_FAILURE, error.response));
+    });
+};
+
+/*------------------
+  addSmurf
+------------------*/
+specials.addSmurf = () => (dispatch, getState) => {
+  const { smurf } = getState ();
+
+  dispatch (make (types.POST_SMURF_TRY, smurf));
+  axios
+    .post (api.smurf.post (), smurf)
+    .then ((response) => {
+      dispatch (make (types.POST_SMURF_SUCCESS, response.data));
+    })
+    .catch ((error) => {
+      dispatch (make (types.POST_SMURF_FAILURE, error.response));
+    });
+};
+
+/*------------------
+  editSmurf
+------------------*/
+specials.editSmurf = () => (dispatch, getState) => {
+  const { smurf } = getState ();
+
+  dispatch (make (types.PUT_SMURF_TRY, smurf));
+  axios
+    .get (api.smurf.put (smurf.id), smurf)
+    .then ((response) => {
+      dispatch (make (types.PUT_SMURF_SUCCESS, response.data));
+    })
+    .catch ((error) => {
+      dispatch (make (types.PUT_SMURF_FAILURE, error.response));
+    });
+};
+
+/*------------------
+  deleteSmurf
+------------------*/
+specials.deleteSmurf = () => (dispatch, getState) => {
+  const { smurf } = getState ();
+
+  dispatch (make (types.DELETE_SMURF_TRY, smurf));
+  axios
+    .get (api.smurf.delete (smurf.id), smurf)
+    .then ((response) => {
+      dispatch (make (types.DELETE_SMURF_SUCCESS, response.data));
+    })
+    .catch ((error) => {
+      dispatch (make (types.DELETE_SMURF_FAILURE, error.response));
+    });
+};
+
 /**************************************/
 
 export default {
   types,
   makers,
+  specials,
 };

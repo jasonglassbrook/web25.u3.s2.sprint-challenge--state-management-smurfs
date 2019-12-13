536f192555ec8bdf932d7331ad26829493c6e015
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-13 13:34:17 -0500

    create + style Smurf

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -1,12 +1,71 @@
 /// external modules ///
 import React from 'react';
+import styled from 'styled-components'
 
 /***************************************
   MAIN
 ***************************************/
-const Smurf = (props) => {
+const Item = styled.li `
+  flex: 1 0 auto;
+  margin: 4px;
+  border: 1px solid White;
+  border-radius: 8px;
+  padding: 4px;
+  color: White;
+  background: SkyBlue;
+`;
+
+const DataList = styled.ul `
+  display: flex;
+  flex-flow: row nowrap;
+  align-content: center;
+  justify-content: center;
+  align-items: center;
+`;
+
+const DataItem = styled.li `
+  flex: 1 0 auto;
+  margin: 4px;
+  border: 1px solid White;
+  border-radius: 8px;
+  padding: 4px;
+  color: White;
+
+  display: flex;
+  flex-flow: row-nowrap;
+`;
+
+const DataItemKey = styled.span `
+  margin: 2px;
+  border-radius: 2px;
+  padding: 2px;
+  color: White;
+`
+
+const DataItemValue = styled.span `
+  margin: 2px;
+  border-radius: 2px;
+  padding: 2px;
+  color: SkyBlue;
+  background: White;
+`
+
+const Smurf = ({ smurf, props }) => {
   return (
-    <div className='Smurf'>Smurf</div>
+    <Item className='Smurf'><DataList>
+      <DataItem>
+        <DataItemKey>name</DataItemKey>
+        <DataItemValue>{smurf.name}</DataItemValue>
+      </DataItem>
+      <DataItem>
+        <DataItemKey>age</DataItemKey>
+        <DataItemValue>{smurf.age}</DataItemValue>
+      </DataItem>
+      <DataItem>
+        <DataItemKey>height</DataItemKey>
+        <DataItemValue>{smurf.height}</DataItemValue>
+      </DataItem>
+    </DataList></Item>
   );
 };
 

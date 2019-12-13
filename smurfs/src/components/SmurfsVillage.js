e3a974025ba87ee15c72f92cdaa80a651cc9132e
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-13 13:42:42 -0500

    build out SmurfsVillage

--- smurfs/src/components/SmurfsVillage.js
+++ smurfs/src/components/SmurfsVillage.js
@@ -1,12 +1,56 @@
 /// external modules ///
 import React from 'react';
+import styled from 'styled-components';
+
+/// components ///
+import Smurf from './Smurf';
 
 /***************************************
   MAIN
 ***************************************/
-const SmurfsVillage = (props) => {
+const Section = styled.section `
+  margin: 8px;
+  border: 4px solid White;
+  border-radius: 16px;
+  padding: 8px;
+  color: White;
+  background: none;
+`;
+
+const SectionHead = styled.header `
+  display: flex;
+  flex-flow: column nowrap;
+  align-content: center;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Heading = styled.h2 ``;
+
+const SectionBody = styled.main ``;
+
+const SmurfsList = styled.ul `
+  display: flex;
+  flex-flow: column nowrap;
+  align-content: center;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SmurfsVillage = ({ village = [], ...props }) => {
   return (
-    <div className='SmurfsVillage'>SmurfsVillage</div>
+    <Section className='SmurfsVillage'>
+      <SectionHead>
+        <Heading>Smurfs Village</Heading>
+      </SectionHead>
+      <SectionBody>
+        <SmurfsList>
+          {village.map ((smurf) => (
+            <Smurf key={smurf.id} smurf={smurf}/>
+          ))}
+        </SmurfsList>
+      </SectionBody>
+    </Section>
   );
 };
 

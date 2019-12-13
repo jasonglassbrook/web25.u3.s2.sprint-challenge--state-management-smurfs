6521e3eacec8dd28c8a68512b35aac29f001dbd3
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-13 12:10:57 -0500

    hook up redux in index.js

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,9 +1,10 @@
 /// external modules ///
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-/// internal modules ///
-import App from './App';
+import * as redux from 'redux';
+import * as ReactRedux from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 /// styles ///
 import 'reset-css/reset.css';
@@ -11,10 +12,27 @@ import 'normalize-css/normalize.css';
 import './styles/o11-basics.css';
 import './styles/index.css';
 
+/// components ///
+import App from './App';
+
+/// states ///
+import smurfs from 'states/smurfs';
+
 /***************************************
   APP
 ***************************************/
+
+const compose = (
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || redux.compose
+);
+
+const smurfsStore = smurfs.store.create (
+  compose (redux.applyMiddleware (thunk, logger))
+);
+
 ReactDOM.render (
-  <App />
+  <ReactRedux.Provider store={smurfsStore}>
+    <App/>
+  </ReactRedux.Provider>
 , document.getElementById ('root')
 );
